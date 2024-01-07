ed63c96c8901ffa08cd2e9fbbccb9dfb36ddb771
watcharin001 <watcharin.nuengkaew@gmail.com>
2024-01-07 21:32:34 +0700

    update path url

--- movie/src/hook/useMovieData.js
+++ movie/src/hook/useMovieData.js
@@ -1,5 +1,5 @@
 import useSWR from 'swr'
-const API_URL = "http://52.62.29.204:8000/api/v1/movie";
+const API_URL = "http://18.138.251.59:8000/api/v1/movie";
 export const useMovies = ()=>{
     let movies = [];
     const fetcher = (...args) => fetch(...args).then((res) => res.json());
